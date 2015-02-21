b82c28d07ef7d683cb29f7f6fcb777497e92af05
jayjhan8 <jayjhan8@gmail.com>
2015-02-21 15:05:44 -0500

    Start

---
+++ Alpha/Assets/LinearPlatform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinearPlatform : MonoBehaviour {
+
+	private Vector3 screenpoint;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDrag() {
+		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		Vector3 curPos = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPos;
+
+	}
+
+	void OnMouseDown() {
+		offset = Camera.main.WorldToScreenPoint(gameObject.transform.position)
+			- Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, gameObject.transform.position.y, gameObject.transform.position.z));
+	}
+
+}
