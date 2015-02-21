d2cbb80d45c3303a2458a7cc1a9f46bc02806899
jayjhan8 <jayjhan8@gmail.com>
2015-02-21 18:00:53 -0500

    hi

--- Alpha/Assets/LinearPlatform.cs
+++ Alpha/Assets/LinearPlatform.cs
@@ -9,25 +9,33 @@ public class LinearPlatform : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-
-	void OnMouseDrag() {
-		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, gameObject.transform.position.y, gameObject.transform.position.z);
-		Vector3 curPos = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-		transform.position = curPos;
 
 	}
-
 	void OnMouseDown() {
-		offset = Camera.main.WorldToScreenPoint(gameObject.transform.position)
-			- Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, gameObject.transform.position.y, gameObject.transform.position.z));
+		screenpoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		offset = gameObject.transform.position
+			- Camera.main.ScreenToWorldPoint(new Vector3(screenpoint.x, Input.mousePosition.y, screenpoint.z));
+//		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (screenpoint);
+
+//		Vector3 yay = new Vector3 (0, screenpoint.y, 0);
+//		offset = screenpoint - new Vector3 (0, Input.mousePosition.y, 0);
+
+		print ("On Mouse Down" + offset);
 	}
-	
-	//test change
+	void OnMouseDrag() {
+		Vector3 curScreenPoint = new Vector3 (screenpoint.x, Input.mousePosition.y , screenpoint.z);
+		Vector3 curPos = Camera.main.ScreenToWorldPoint(curScreenPoint + offset);
+//		Vector3 curPos = transform.position;
+//		curPos += new Vector3(0, Input.mousePosition.y
+		transform.position = curPos;
+//		print ("On Mouse Drag, Before: " + Camera.main.ScreenToWorldPoint(curScreenPoint) + "After: " + curPos);
+		print (screenpoint);
+	}
+
 
 }
