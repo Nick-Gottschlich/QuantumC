b23db9c040c9f63f94a664c53321fcd3e41f7502
jayjhan8 <jayjhan8@gmail.com>
2015-02-24 16:13:37 -0500

    Modifications

---
+++ Alpha/Assets/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Variable to determine which player
+	public bool 		player1or2;
+
+	public float 		speed;
+	public Vector3		vel;
+
+	// Use this for initialization
+	void Start () {
+		vel = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xdirection1 = Input.GetAxisRaw("Horizontal1");
+		float xdirection2 = Input.GetAxisRaw ("Horizontal2");
+		float ydirection1 = Input.GetAxisRaw ("Vertical1");
+		float ydirection2 = Input.GetAxisRaw ("Vertical2");
+
+		// Player 1
+		if (!player1or2) {
+			vel.x = xdirection * speed;
+			vel.y = ydirection1 * speed;
+		} 
+		// Player 2
+		else {
+		}
+
+
+		rigidbody.velocity = vel;
+
+	}
+}
