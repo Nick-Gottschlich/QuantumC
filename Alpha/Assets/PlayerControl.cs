9d8f1d7faa182db3d53cc6dca11d3c1c004bce36
jayjhan8 <jayjhan8@gmail.com>
2015-02-25 12:39:43 -0500

    quads complete

--- Alpha/Assets/PlayerControl.cs
+++ Alpha/Assets/PlayerControl.cs
@@ -8,6 +8,8 @@ public class PlayerControl : MonoBehaviour {
 
 	public float 		speed;
 	public Vector3		vel;
+	public Collider		nextPad;
+	public Collider		curPad;
 
 	// Use this for initialization
 	void Start () {
@@ -16,22 +18,95 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float xdirection1 = Input.GetAxisRaw("Horizontal1");
-		float xdirection2 = Input.GetAxisRaw ("Horizontal2");
-		float ydirection1 = Input.GetAxisRaw ("Vertical1");
-		float ydirection2 = Input.GetAxisRaw ("Vertical2");
+
+		Collider[] hitColliders = Physics.OverlapSphere (transform.position, 1.5f);
 
 		// Player 1
 		if (!player1or2) {
-			vel.x = xdirection * speed;
-			vel.y = ydirection1 * speed;
+			float xdirection1 = Input.GetAxisRaw("Horizontal1");
+			float zdirection1 = Input.GetAxisRaw ("Vertical1");
+
+
+			// Find the current pad its on
+			foreach (Collider nearcollider in hitColliders) {
+				if (Mathf.Abs(nearcollider.transform.position.x - this.gameObject.transform.position.x) < .2f &&
+				    Mathf.Abs(nearcollider.transform.position.z - this.gameObject.transform.position.z) < .2f &&
+				    nearcollider.tag == "Pad") {
+					curPad = nearcollider;
+					break;
+				}
+			}
+
+			//Go left
+			if (curPad.GetComponent<Pad>().left && xdirection1 < 0) {
+				foreach (Collider nearcollider in hitColliders) {
+					if (nearcollider.transform.position.x < curPad.transform.position.x &&
+					    nearcollider.transform.position.z == curPad.transform.position.z &&
+					    nearcollider.tag == "Pad") {
+						nextPad = nearcollider;
+						break;
+					}
+				}
+				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
+				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, .15f);
+			}
+
+			//Go right
+			else if (curPad.GetComponent<Pad>().right && xdirection1 > 0) {
+				foreach (Collider nearcollider in hitColliders) {
+					if (nearcollider.transform.position.x > curPad.transform.position.x &&
+					    nearcollider.transform.position.z == curPad.transform.position.z &&
+					    nearcollider.tag == "Pad") {
+						nextPad = nearcollider;
+						break;
+					}
+				}
+				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
+				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, .15f);
+			}
+
+			//Go up
+			else if (curPad.GetComponent<Pad>().up && zdirection1 > 0) {
+				foreach (Collider nearcollider in hitColliders) {
+					if (nearcollider.transform.position.x == curPad.transform.position.x &&
+					    nearcollider.transform.position.z > curPad.transform.position.z &&
+					    nearcollider.tag == "Pad") {
+						nextPad = nearcollider;
+						break;
+					}
+				}
+				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
+				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, .15f);
+
+			}
+
+			//Go down
+			else if (curPad.GetComponent<Pad>().down && zdirection1 < 0) {
+				foreach (Collider nearcollider in hitColliders) {
+					if (nearcollider.transform.position.x == curPad.transform.position.x &&
+					    nearcollider.transform.position.z < curPad.transform.position.z &&
+					    nearcollider.tag == "Pad") {
+						nextPad = nearcollider;
+						break;
+					}
+				}
+				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
+				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, .15f);
+			}
+
+//			vel.x = xdirection1 * speed;
+//			vel.z = ydirection1 * speed;
+
 		} 
 		// Player 2
 		else {
 		}
 
-
 		rigidbody.velocity = vel;
+	}
 
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		//Gizmos.DrawSphere(transform.position, 1.5f);
 	}
 }
