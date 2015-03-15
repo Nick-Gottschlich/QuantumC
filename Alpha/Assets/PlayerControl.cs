e4e8b9957e15d72321b5d27d6e776fb92741dffd
pkabra <pratik@kabra.com>
2015-03-15 17:56:33 -0400

    Add Player and Pad movement stuff

--- Alpha/Assets/PlayerControl.cs
+++ Alpha/Assets/PlayerControl.cs
@@ -6,9 +6,17 @@ public class PlayerControl : MonoBehaviour {
 	// Variable to determine which player
 	public bool 		player1or2;
 
-	public float 		smooth = 2f;
-	public Collider		nextPad;
-	public Collider		curPad;
+	public GameObject	up;
+	public GameObject	down;
+	public GameObject	left;
+	public GameObject	right;
+
+	public float 		smooth = 10f;
+	public Pad			curPad;
+	float 				journeyDistance = 0f;
+	bool 				distanceSet = false;
+
+	float lastMove = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,158 +24,54 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Time.time - lastMove > 0.1f) {
+			// Player 1
+			if (!player1or2) {
+				float xdirection1 = Input.GetAxisRaw("Horizontal1");
+				float zdirection1 = Input.GetAxisRaw ("Vertical1");
 
-		Collider[] hitColliders = Physics.OverlapSphere (transform.position, 1.5f);
+				GameObject searchPos = null;
 
-		// Player 1
-		if (!player1or2) {
-			float xdirection1 = Input.GetAxisRaw("Horizontal1");
-			float zdirection1 = Input.GetAxisRaw ("Vertical1");
-
-
-			// Find the current pad its on
-			foreach (Collider nearcollider in hitColliders) {
-				if (Mathf.Abs(nearcollider.transform.position.x - this.gameObject.transform.position.x) < .2f &&
-				    Mathf.Abs(nearcollider.transform.position.z - this.gameObject.transform.position.z) < .2f &&
-				    nearcollider.tag == "Pad") {
-					curPad = nearcollider;
-					break;
+				if (zdirection1 > 0) {
+					searchPos = up;
 				}
-			}
+				if (zdirection1 < 0) {
+					searchPos = down;
+				}
+				if (xdirection1 < 0) {
+					searchPos = left;
+				}
+				if (xdirection1 > 0) {
+					searchPos = right;
+				}
 
-			//Go left
-			if (curPad.GetComponent<Pad>().left && xdirection1 < 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x < curPad.transform.position.x &&
-					    nearcollider.transform.position.z == curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
+				if (searchPos) {
+					lastMove = Time.time;
+					Collider[] hits = Physics.OverlapSphere(searchPos.transform.position, 0.5f);
+					if (hits.Length > 0) {
+						Pad[] pads = hits[0].GetComponentsInParent<Pad>();
+						curPad = pads[0];
+						distanceSet = false;
 					}
 				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, Time.deltaTime * smooth);
-			}
+			} 
 
-			//Go right
-			else if (curPad.GetComponent<Pad>().right && xdirection1 > 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x > curPad.transform.position.x &&
-					    nearcollider.transform.position.z == curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, Time.deltaTime * smooth);
-			}
+			// Player 2
+			else {
+				float xdirection2 = Input.GetAxisRaw("Horizontal2");
+				float zdirection2 = Input.GetAxisRaw ("Vertical2");
 
-			//Go up
-			else if (curPad.GetComponent<Pad>().up && zdirection1 > 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x == curPad.transform.position.x &&
-					    nearcollider.transform.position.z > curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, Time.deltaTime * smooth);
-
-			}
-
-			//Go down
-			else if (curPad.GetComponent<Pad>().down && zdirection1 < 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x == curPad.transform.position.x &&
-					    nearcollider.transform.position.z < curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, Time.deltaTime * smooth);
-			}
-	
-		} 
-
-		// Player 2
-		else {
-			float xdirection2 = Input.GetAxisRaw("Horizontal2");
-			float zdirection2 = Input.GetAxisRaw ("Vertical2");
-			
-			
-			// Find the current pad its on
-			foreach (Collider nearcollider in hitColliders) {
-				if (Mathf.Abs(nearcollider.transform.position.x - this.gameObject.transform.position.x) < .2f &&
-				    Mathf.Abs(nearcollider.transform.position.z - this.gameObject.transform.position.z) < .2f &&
-				    nearcollider.tag == "Pad") {
-					curPad = nearcollider;
-					break;
-				}
-			}
-			
-			//Go left
-			if (curPad.GetComponent<Pad>().left && xdirection2 < 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x < curPad.transform.position.x &&
-					    nearcollider.transform.position.z == curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, Time.deltaTime * smooth);
-			}
-			
-			//Go right
-			else if (curPad.GetComponent<Pad>().right && xdirection2 > 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x > curPad.transform.position.x &&
-					    nearcollider.transform.position.z == curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, Time.deltaTime * smooth);
-			}
-			
-			//Go up
-			else if (curPad.GetComponent<Pad>().up && zdirection2 > 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x == curPad.transform.position.x &&
-					    nearcollider.transform.position.z > curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, smooth);
-				
-			}
-			
-			//Go down
-			else if (curPad.GetComponent<Pad>().down && zdirection2 < 0) {
-				foreach (Collider nearcollider in hitColliders) {
-					if (nearcollider.transform.position.x == curPad.transform.position.x &&
-					    nearcollider.transform.position.z < curPad.transform.position.z &&
-					    nearcollider.tag == "Pad") {
-						nextPad = nearcollider;
-						break;
-					}
-				}
-				Vector3 newPos = new Vector3(nextPad.transform.position.x, gameObject.transform.position.y, nextPad.transform.position.z);
-				transform.position = Vector3.Lerp(gameObject.transform.position, newPos, smooth);
 			}
 		}
 
+		Vector3 newPos = new Vector3(curPad.transform.position.x, gameObject.transform.position.y, curPad.transform.position.z);
+		if (!distanceSet) {
+			distanceSet = true;
+			journeyDistance = Vector3.Distance(transform.position, newPos);
+		}
+
+		float fracJourney = Time.deltaTime * smooth / journeyDistance;
+		transform.position = Vector3.Lerp (transform.position, newPos, fracJourney);
 	}
 
 	void OnDrawGizmos() {
