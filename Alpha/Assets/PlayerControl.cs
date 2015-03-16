05e9d1cea3e91b3e8745197f0e7ef96b3078d283
pkabra <pratik@kabra.com>
2015-03-15 20:04:59 -0400

    Player mounting

--- Alpha/Assets/PlayerControl.cs
+++ Alpha/Assets/PlayerControl.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	// Variable to determine which player
-	public bool 		player1or2;
+	public bool 			player1or2;
+	public int				playerNum = 1;
 
-	public GameObject	up;
-	public GameObject	down;
-	public GameObject	left;
-	public GameObject	right;
+	public GameObject		up;
+	public GameObject		down;
+	public GameObject		left;
+	public GameObject		right;
 
-	public float 		smooth = 10f;
-	public Pad			curPad;
-	float 				journeyDistance = 0f;
-	bool 				distanceSet = false;
+	public float 			smooth = 10f;
+	public Pad				curPad;
+	float 					journeyDistance = 0f;
+	bool 					distanceSet = false;
+
+	public PlayerControl	carrying;
+	public PlayerControl	carriedBy;
+
+	public Pad 				head;
 
 	float lastMove = 0f;
 
@@ -26,45 +32,56 @@ public class PlayerControl : MonoBehaviour {
 	void Update () {
 		if (Time.time - lastMove > 0.1f) {
 			// Player 1
-			if (!player1or2) {
-				float xdirection1 = Input.GetAxisRaw("Horizontal1");
-				float zdirection1 = Input.GetAxisRaw ("Vertical1");
+			float xdirection = Input.GetAxisRaw("Horizontal" + playerNum);
+			float zdirection = Input.GetAxisRaw ("Vertical" + playerNum);
 
-				GameObject searchPos = null;
+			GameObject searchPos = null;
 
-				if (zdirection1 > 0) {
-					searchPos = up;
-				}
-				if (zdirection1 < 0) {
-					searchPos = down;
-				}
-				if (xdirection1 < 0) {
-					searchPos = left;
-				}
-				if (xdirection1 > 0) {
-					searchPos = right;
-				}
+			if (zdirection > 0) {
+				searchPos = up;
+			}
+			if (zdirection < 0) {
+				searchPos = down;
+			}
+			if (xdirection < 0) {
+				searchPos = left;
+			}
+			if (xdirection > 0) {
+				searchPos = right;
+			}
 
-				if (searchPos) {
-					lastMove = Time.time;
-					Collider[] hits = Physics.OverlapSphere(searchPos.transform.position, 0.5f);
-					if (hits.Length > 0) {
-						Pad[] pads = hits[0].GetComponentsInParent<Pad>();
-						curPad = pads[0];
+			if (searchPos) {
+				lastMove = Time.time;
+				Vector3 startPos = searchPos.transform.position;
+				if (carriedBy) startPos.y -= 0.6f;
+				Collider[] hits = Physics.OverlapSphere(startPos, 0.5f);
+				if (hits.Length > 0) {
+					Pad pad = null;
+					bool hasPlayer = false;
+					print (hits.Length);
+					foreach (Collider c in hits) {
+						print (c.tag);
+						if (c.CompareTag("Player")) {
+							Mount(c);
+							hasPlayer = true;
+						} else if (c.CompareTag("Pad")) {
+							pad = c.GetComponentInParent<Pad>();
+							print (pad);
+						}
+					}
+					if (!hasPlayer && pad) {
+						curPad = pad;
+						if (carriedBy) {
+							carriedBy.carrying = null;
+							carriedBy = null;
+						}
 						distanceSet = false;
 					}
 				}
-			} 
-
-			// Player 2
-			else {
-				float xdirection2 = Input.GetAxisRaw("Horizontal2");
-				float zdirection2 = Input.GetAxisRaw ("Vertical2");
-
 			}
 		}
 
-		Vector3 newPos = new Vector3(curPad.transform.position.x, gameObject.transform.position.y, curPad.transform.position.z);
+		Vector3 newPos = new Vector3(curPad.transform.position.x, curPad.transform.position.y + 0.6f, curPad.transform.position.z);
 		if (!distanceSet) {
 			distanceSet = true;
 			journeyDistance = Vector3.Distance(transform.position, newPos);
@@ -74,8 +91,10 @@ public class PlayerControl : MonoBehaviour {
 		transform.position = Vector3.Lerp (transform.position, newPos, fracJourney);
 	}
 
-	void OnDrawGizmos() {
-		Gizmos.color = Color.red;
-		//Gizmos.DrawSphere(transform.position, 1.5f);
+	void Mount(Collider c) {
+		PlayerControl player = c.GetComponent<PlayerControl>();
+		player.carrying = this;
+		carriedBy = player;
+		curPad = player.head;
 	}
 }
