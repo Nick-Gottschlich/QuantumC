d3cb477a07ed37dc3944cfceff643994d759cd60
jayjhan8 <jayjhan8@gmail.com>
2015-03-15 21:29:08 -0400

    button added

--- Alpha/Assets/LinearPlatform.cs
+++ Alpha/Assets/LinearPlatform.cs
@@ -35,37 +35,51 @@ public class LinearPlatform : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-
-		if (rotation) {
-//			if (trigger.GetComponent<Pad>().rotate_activate == true) {
-//				Rotate();	
+//		if (rotation) {
+////			if (trigger.GetComponent<Pad>().rotate_activate == true) {
+////				Rotate();	
+////			}
+//			if (rotationAxis.Equals(Axis.x)) {
+//				correctPosition = transform.eulerAngles.x <= correctAngle+0.1f && transform.eulerAngles.x >= correctAngle-0.1f;
+//			} else if (rotationAxis.Equals(Axis.y)) {
+//				correctPosition = transform.eulerAngles.y <= correctAngle+0.1f && transform.eulerAngles.y >= correctAngle-0.1f;
+//			} else if (rotationAxis.Equals(Axis.z)) {
+//				correctPosition = transform.eulerAngles.z <= correctAngle+0.1f && transform.eulerAngles.z >= correctAngle-0.1f;
 //			}
-
-			if (rotationAxis.Equals(Axis.x)) {
-				correctPosition = transform.eulerAngles.x <= correctAngle+0.1f && transform.eulerAngles.x >= correctAngle-0.1f;
-			} else if (rotationAxis.Equals(Axis.y)) {
-				correctPosition = transform.eulerAngles.y <= correctAngle+0.1f && transform.eulerAngles.y >= correctAngle-0.1f;
-			} else if (rotationAxis.Equals(Axis.z)) {
-				correctPosition = transform.eulerAngles.z <= correctAngle+0.1f && transform.eulerAngles.z >= correctAngle-0.1f;
-			}
-
-		}
+//
+//		}
 		transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, Time.deltaTime * rotationSpeed);
 	}
 
-	public void Rotate() {
+	public void Rotate(Axis xyz) {
 		float angle = 90f;
-		if (rotationAxis.Equals(Axis.x)) {
+		if (xyz == Axis.x) {
 			toRot *= Quaternion.Euler (angle, 0f, 0f);
-		} else if (rotationAxis.Equals(Axis.y)) {
+		} else if (xyz == Axis.y) {
 			toRot *= Quaternion.Euler (0f, angle, 0f);
-		} else if (rotationAxis.Equals(Axis.z)) {
+		} else if (xyz == Axis.z) {
 			toRot *= Quaternion.Euler (0f, 0f, angle);
 		}
-
-		//transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, Time.deltaTime * rotationSpeed);
 	}
 
-
+	public void Move(Axis xyz, bool posOrNeg) {
+		
+		//go right
+		if (posOrNeg && xyz == Axis.x) {
+			transform.Translate (Vector3.up * Time.deltaTime);
+		} else if (!posOrNeg && xyz == Axis.x) {
+			transform.Translate (Vector3.up * Time.deltaTime);
+		} else if (posOrNeg && xyz == Axis.y) {
+			transform.Translate (Vector3.up * Time.deltaTime);
+		} else if (!posOrNeg && xyz == Axis.y) {
+			transform.Translate (Vector3.up * Time.deltaTime);
+		} else if (posOrNeg && xyz == Axis.z) {
+			transform.Translate (Vector3.up * Time.deltaTime);
+		} else if (!posOrNeg && xyz == Axis.z) {
+			transform.Translate (Vector3.up * Time.deltaTime);
+		} else {
+			print ("ew");		
+		}
+		
+	}
 }
