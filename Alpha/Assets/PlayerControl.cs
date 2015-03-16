134856bfebed010b4e26b0408ad8d07606e9a787
NRBond7 <nrbond7@gmail.com>
2015-03-15 21:00:28 -0400

    Updated Player control for different plane detection.

--- Alpha/Assets/PlayerControl.cs
+++ Alpha/Assets/PlayerControl.cs
@@ -24,12 +24,24 @@ public class PlayerControl : MonoBehaviour {
 
 	float lastMove = 0f;
 
+	public char plane = 'Y';
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (transform.rotation.z == 90 || transform.rotation.z == 270)
+			plane = 'X';
+		else if (transform.rotation.z == 0 && transform.rotation.x == 0)
+			plane = 'Y';
+		else if (transform.rotation.x == 90 || transform.rotation.x == 270)
+			plane = 'Z';
+		//else{
+		//		add in code to keep the player fixed to the gamepad when it is on a rotating platform
+		//}
+
 		if (Time.time - lastMove > 0.1f) {
 			// Player 1
 			float xdirection = Input.GetAxisRaw("Horizontal" + playerNum);
@@ -37,6 +49,7 @@ public class PlayerControl : MonoBehaviour {
 
 			GameObject searchPos = null;
 
+<<<<<<< HEAD
 			if (zdirection > 0) {
 				searchPos = up;
 			}
@@ -75,13 +88,46 @@ public class PlayerControl : MonoBehaviour {
 							carriedBy.carrying = null;
 							carriedBy = null;
 						}
+=======
+				if (zdirection1 > 0) {
+					searchPos = up;
+				}
+				if (zdirection1 < 0) {
+					searchPos = down;
+				}
+				if (xdirection1 < 0) {
+					searchPos = left;
+				}
+				if (xdirection1 > 0) {
+					searchPos = right;
+				}
+				
+				if (searchPos) {
+					lastMove = Time.time;
+					Collider[] hits = Physics.OverlapSphere(searchPos.transform.position, 0.5f);
+					if (hits.Length > 0) {
+						Pad[] pads = hits[0].GetComponentsInParent<Pad>();
+						curPad = pads[0];
+>>>>>>> Updated Player control for different plane detection.
 						distanceSet = false;
 					}
 				}
 			}
 		}
 
+<<<<<<< HEAD
 		Vector3 newPos = new Vector3(curPad.transform.position.x, curPad.transform.position.y + 0.6f, curPad.transform.position.z);
+=======
+		Vector3 newPos;
+
+		if (plane == 'X') {
+			newPos = new Vector3 (gameObject.transform.position.x, curPad.transform.position.y, curPad.transform.position.z);
+		} else if (plane == 'Y') {
+			newPos = new Vector3 (curPad.transform.position.x, gameObject.transform.position.y, curPad.transform.position.z);
+		} else{
+			newPos = new Vector3 (curPad.transform.position.x, curPad.transform.position.y, gameObject.transform.position.z);
+		}
+>>>>>>> Updated Player control for different plane detection.
 		if (!distanceSet) {
 			distanceSet = true;
 			journeyDistance = Vector3.Distance(transform.position, newPos);
@@ -96,5 +142,14 @@ public class PlayerControl : MonoBehaviour {
 		player.carrying = this;
 		carriedBy = player;
 		curPad = player.head;
+	}
+
+	void changePlane(int p){
+		if (p == 0)
+			plane = 'X';
+		else if (p == 1)
+			plane = 'Y';
+		else if (p == 2)
+			plane = 'Z';
 	}
 }
