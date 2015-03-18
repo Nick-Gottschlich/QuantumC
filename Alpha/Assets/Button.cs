f20b9a088ccde534e5193907f2cce969800d1891
jayjhan8 <jayjhan8@gmail.com>
2015-03-18 01:36:12 -0400

    Pratik did everything

--- Alpha/Assets/Button.cs
+++ Alpha/Assets/Button.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum MoveOrRotate {Move, Rotate};
+
 public class Button : MonoBehaviour {
 
-	public LinearPlatform rotateObject;
-	public bool pressed, rotation, move, posOrNeg;
-	public Axis rotateDir, moveDir;
-	public Vector3 threshold;
+	public LinearPlatform controlObject;
+	public MoveOrRotate moveOrRotate = MoveOrRotate.Rotate;
+	public bool pressed = false;
 
 	// For Debugging purposes
 	void OnGizmosDraw() {
@@ -14,44 +15,28 @@ public class Button : MonoBehaviour {
 		Gizmos.DrawLine (transform.position, transform.position + .3f * Vector3.up);
 
 		Gizmos.color = Color.green;
-		Gizmos.DrawLine (transform.position, rotateObject.transform.position);
+		Gizmos.DrawLine (transform.position, controlObject.transform.position);
 	}
 
 	// Use this for initialization
 	void Start () {
-		pressed = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		// the instant the button is pressed
-		if (Physics.Raycast (transform.position, Vector3.up, 1f) && pressed == false) {
+		int layerMask = 1 << LayerMask.NameToLayer ("PlayerLayer");
+		Vector3 rayCenter = transform.position;
+		rayCenter.y -= 0.5f;
+		if (Physics.Raycast (rayCenter, Vector3.up, 1f, layerMask) && !pressed) {
 			pressed = true;
-
-			// Rotation
-			if (rotation) {
-				rotateObject.Rotate(rotateDir);
-			}
-
-
-		} 
-		// as long as the player stands on top of the button
-		else if (Physics.Raycast (transform.position, Vector3.up, 1f)) {
-			pressed = true;
-		}
-		else {
+			if (moveOrRotate == MoveOrRotate.Move)
+				controlObject.Move ();
+			else
+				controlObject.Rotate();
+		} else if(!Physics.Raycast (rayCenter, Vector3.up, 1f, layerMask)) {
 			pressed = false;
-		}
-
-		if (move && pressed) {
-			// Translation
-			if (move) {
-				rotateObject.transform.Translate(Vector3.up * Time.deltaTime);
-				if (Vector3.Distance(rotateObject.transform.position,threshold) < .1f) {
-					move = false;
-				}
-			}		
 		}
 	}
 }
--- Alpha/Assets/LinearPlatform.cs
+++ Alpha/Assets/LinearPlatform.cs
@@ -5,22 +5,19 @@ public enum Axis {x, y, z}
 
 public class LinearPlatform : MonoBehaviour {
 
-	public GameObject trigger;
+	bool posOrNeg = false;
 
-	private Vector3 screenpoint;
-	private Vector3 offset;
+	public Axis rotationAxis;
+	public float rotspeed = 50f;
+	float angle = 0f;
 
-	Vector3 dragStartPosition;
-	float dragStartDistance;
-
-	public bool rotation = false;
-	public Axis rotationAxis = Axis.x;
-	public float rotationSpeed = 50f;
-	public float correctAngle = 0f;
-	public static bool correctPosition = false;
+	public Axis moveAxis;
+	Vector3 startPos;
+	public Vector3 finalPos;
+	public float movspeed = 10f;
+	
 	Quaternion toRot = Quaternion.identity;
-
-
+	
 	// Gizmos
 	void OnDrawGizmos() {
 //		Gizmos.color = Color.green;
@@ -31,55 +28,58 @@ public class LinearPlatform : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		toRot = transform.rotation;
+		if (rotationAxis == Axis.x) {
+			angle = transform.eulerAngles.x;
+		} else if (rotationAxis == Axis.y) {
+			angle = transform.eulerAngles.y;
+		} else if (rotationAxis == Axis.z) {
+			angle = transform.eulerAngles.z;
+		}
+		startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (rotation) {
-////			if (trigger.GetComponent<Pad>().rotate_activate == true) {
-////				Rotate();	
-////			}
-//			if (rotationAxis.Equals(Axis.x)) {
-//				correctPosition = transform.eulerAngles.x <= correctAngle+0.1f && transform.eulerAngles.x >= correctAngle-0.1f;
-//			} else if (rotationAxis.Equals(Axis.y)) {
-//				correctPosition = transform.eulerAngles.y <= correctAngle+0.1f && transform.eulerAngles.y >= correctAngle-0.1f;
-//			} else if (rotationAxis.Equals(Axis.z)) {
-//				correctPosition = transform.eulerAngles.z <= correctAngle+0.1f && transform.eulerAngles.z >= correctAngle-0.1f;
-//			}
-//
-//		}
-		transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, Time.deltaTime * rotationSpeed);
+		transform.rotation = DoRotation ();
+		transform.position = DoMove ();
 	}
 
-	public void Rotate(Axis xyz) {
-		float angle = 90f;
-		if (xyz == Axis.x) {
-			toRot *= Quaternion.Euler (angle, 0f, 0f);
-		} else if (xyz == Axis.y) {
-			toRot *= Quaternion.Euler (0f, angle, 0f);
-		} else if (xyz == Axis.z) {
-			toRot *= Quaternion.Euler (0f, 0f, angle);
+	Quaternion DoRotation () {
+		if (rotationAxis == Axis.x) {
+			toRot = Quaternion.Euler (angle, 0f, 0f);
+		} else if (rotationAxis == Axis.y) {
+			toRot = Quaternion.Euler (0f, angle, 0f);
+		} else if (rotationAxis == Axis.z) {
+			toRot = Quaternion.Euler (0f, 0f, angle);
 		}
+		return Quaternion.RotateTowards(transform.rotation, toRot, Time.deltaTime * rotspeed);
 	}
 
-	public void Move(Axis xyz, bool posOrNeg) {
-		
-		//go right
-		if (posOrNeg && xyz == Axis.x) {
-			transform.Translate (Vector3.up * Time.deltaTime);
-		} else if (!posOrNeg && xyz == Axis.x) {
-			transform.Translate (Vector3.up * Time.deltaTime);
-		} else if (posOrNeg && xyz == Axis.y) {
-			transform.Translate (Vector3.up * Time.deltaTime);
-		} else if (!posOrNeg && xyz == Axis.y) {
-			transform.Translate (Vector3.up * Time.deltaTime);
-		} else if (posOrNeg && xyz == Axis.z) {
-			transform.Translate (Vector3.up * Time.deltaTime);
-		} else if (!posOrNeg && xyz == Axis.z) {
-			transform.Translate (Vector3.up * Time.deltaTime);
-		} else {
-			print ("ew");		
+	public void Rotate() {
+		angle += 90f;
+	}
+
+	Vector3 DoMove () {
+		Vector3 dest = posOrNeg ? finalPos : startPos;
+		if (Vector3.Distance (transform.position, dest) > 0.2f) {
+			if (moveAxis == Axis.x && posOrNeg) {
+				return transform.position + Vector3.forward * Time.deltaTime * movspeed;
+			} else if (moveAxis == Axis.x && !posOrNeg) {
+				return transform.position + Vector3.back * Time.deltaTime * movspeed;
+			} else if (moveAxis == Axis.y && posOrNeg) {
+				return transform.position + Vector3.up * Time.deltaTime * movspeed;
+			} else if (moveAxis == Axis.y && !posOrNeg) {
+				return transform.position + Vector3.down * Time.deltaTime * movspeed;
+			} else if (moveAxis == Axis.z && posOrNeg) {
+				return transform.position + Vector3.left * Time.deltaTime * movspeed;
+			} else if (moveAxis == Axis.z && !posOrNeg) {
+				return transform.position + Vector3.right * Time.deltaTime * movspeed;
+			}
 		}
-		
+		return dest;
+	}
+
+	public void Move() {
+		posOrNeg = !posOrNeg;
 	}
 }
