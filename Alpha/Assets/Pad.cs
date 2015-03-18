b4174a0f329d61b4596edac66aad94c0c1d4086a
jayjhan8 <jayjhan8@gmail.com>
2015-03-17 20:35:33 -0400

    before gold spike, need slope pads

--- Alpha/Assets/Pad.cs
+++ Alpha/Assets/Pad.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class Pad : MonoBehaviour {
-	
+
+	public bool teleport;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (teleport) {
+					
+		}
+	}
+	void OnGizmosDraw() {
+		if (teleport) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere (transform.position, 1f);
+		}
 	}
 }
