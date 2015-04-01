ffe5484fd370c398d2a1da4c97d2cca66a33212d
jayjhan8 <jayjhan8@gmail.com>
2015-04-01 14:54:55 -0400

    why didn't you carry over?

---
+++ Alpha/Assets/Scripts/Lava.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Restart level
+		if (transform.position.y > 7.2f)
+			Application.LoadLevel(Application.loadedLevel);
+	
+	}
+
+	// Edited in Project settings that it only 
+	// collides with player!
+	void OnCollisionEnter(Collision c) {
+
+		// Restart level
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
